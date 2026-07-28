8a75248de16034695e3ea45b642611d542e2ca86
Andrew Stowell <andrew@digitaladapt.com>
2026-07-28 15:59:33 -0400

    created a logo

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@
 vitals_dir=$(readlink -f "$0" | xargs dirname)
 
 mkdir -p "$HOME/host/caddy/data/data/vitals"
-cp "$vitals_dir/public/app.js"      "$HOME/host/caddy/data/data/vitals/"
-cp "$vitals_dir/public/chart-v4.js" "$HOME/host/caddy/data/data/vitals/"
-cp "$vitals_dir/public/index.html"  "$HOME/host/caddy/data/data/vitals/"
+cp -r "$vitals_dir"/public/* "$HOME/host/caddy/data/data/vitals/"
